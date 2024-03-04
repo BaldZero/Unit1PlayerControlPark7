d6751ff6175aa2f146b86bf0896fbf897691d157
Hyunmin Park <753429@student.dusd.net>
2024-03-04 15:29:19 -0800

    Challenge 1
    finished unit 1 and bonus feaatures and started challenge 1. Fixed speed and moving bakwards plane

--- PlayerControl7Unit1/Assets/Scripts/PlayerFollower.cs
+++ PlayerControl7Unit1/Assets/Scripts/PlayerFollower.cs
@@ -7,11 +7,14 @@ public class PlayerFollower : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -8);
+    public Camera cameran;
+    public Camera camel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameran.enabled = false;
+        camel.enabled = true;
     }
 
     // Update is called once per frame
@@ -19,6 +22,17 @@ public class PlayerFollower : MonoBehaviour
     {
         //Offset the camera behind the vehicle by adding a new vector3 with the cameras old position.
         transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            camel.enabled = false;
+            cameran.enabled = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            camel.enabled = true;
+            cameran.enabled = false;
+        }
     }
 
 }
