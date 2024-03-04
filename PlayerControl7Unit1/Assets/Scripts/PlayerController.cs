7fa4ebd9634ae7f71a0f006e94d891994fc910f0
Hyunmin Park <753429@student.dusd.net>
2024-03-04 10:01:25 -0800

    Finished carls videos and started bonuys features
    finished 1.4 and started bonus features. added pyramids and other vehicles.

--- PlayerControl7Unit1/Assets/Scripts/PlayerController.cs
+++ PlayerControl7Unit1/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +21,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        //We will move the car forward
+        //Car moves forward/backward depending on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Car rotates depending on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
     }
