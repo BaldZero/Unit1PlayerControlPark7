256166f3b283f1f6486d38e2374f854856a8d6b1
Hyunmin Park <753429@student.dusd.net>
2024-03-06 15:27:44 -0800

    camera on side now
    on side now

--- PlayerControl7Unit1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlayerControl7Unit1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3 (0, 5, -15);
+    private Vector3 offset = new Vector3 (22, 5, 0);
 
     // Start is called before the first frame update
     void Start()
